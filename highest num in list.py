9c77ca459595cf25c86b97e431a6093dec153309
jacko8638 <jdowning@kingsworcester.org.uk>
2018-01-15 10:12:17 +0000

    Initial commit

---
+++ highest num in list.py
@@ -0,0 +1,12 @@
+def findHighest(list):
+    if len(list) == 0:
+        return -1
+    else:
+        high = list[0]
+        for num in list:
+            if num > high:
+                high = num
+        return high
+
+numList=(1,2,3,4,5,6,7,8,9,234,546,215,1456,13,5367,135,245,54678,13425)
+print(findHighest(numList))

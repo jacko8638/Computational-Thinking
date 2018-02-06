41f93345922396f2296ecc4e0df7190f9fa9474a
jacko8638 <jdowning@kingsworcester.org.uk>
2018-02-06 12:53:51 +0000

    yes

--- highest num in list.py
+++ highest num in list.py
@@ -1,4 +1,5 @@
 def findHighest(list):
+    #subroutine that finds the highest num in a list
     if len(list) == 0:
         return "THE LIST IS EMPTY!!!! ARE YOU HAPPY NOW?!"
     else:
@@ -8,5 +9,8 @@ def findHighest(list):
                 high = num
         return high
 
+#list for testing
 numList=(1,2,3,4,5,6,7,8,9,234,546,215,1456,13,5367,135,245,54678,13425)
+
+#main program
 print(findHighest(numList))

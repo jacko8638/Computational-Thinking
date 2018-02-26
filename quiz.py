8d39a0270a6f5d3742104232563444ecdd6acae6
jacko8638 <jdowning@kingsworcester.org.uk>
2018-02-26 00:15:56 +0000

    I should have done this earlier

--- quiz.py
+++ quiz.py
@@ -3,7 +3,9 @@ from time import sleep
 
 def question1():
     global totalpoints
+    global QIClaxon
     #A is the correct answer
+
     #this prints the question and answers
     sleep(1)
     print("what is the best IT related tv show")
@@ -18,26 +20,34 @@ def question1():
     sleep(1)
     print("D: </scorpion>")
     sleep(1)
+
     #this asks the user to input their answer
     answer = input("Your answer:")
-    #first if statement questions if answer is valid
+
+    #if statement questions if answer is valid
     if len(answer) == 1 and answer.isalpha():
         #converts answer to always be lower case so it can be understood by if statement
         answer.lower()
+
         if answer == "a":
             print("Congrats, your answer is correct!")
             #updates points
             totalpoints = totalpoints + 1
+
         elif answer == "b":
+            QIClaxon = "ON"
             #corrects user who thinks silicon valley is better than IT Crowd
             print("Thats definatly wrong, for that answer your getting two negative points")
             #updates points
             totalpoints = totalpoints - 2
+
         else:
             print("Oh no! Your answer was incorrect")
             #updates points
             totalpoints = totalpoints - 1
+
     #calls next question
+
     else:
         #gets user to re-enter answer if it wasnt a valid answer
         print("Sorry I didn't understand your answer, please try again")
@@ -47,6 +57,7 @@ def question1():
 
 #globals
 totalpoints = 0
+QIClaxon = "OFF"
 
 
 
@@ -54,5 +65,5 @@ totalpoints = 0
 print("Welcome to the quiz.")
 question1()
 print("Thank you for playing")
-#displays how many answers they got right and wrong
-print("Your score is %d" % totalpoints
+#displays how many points they get
+print("Your score is: %d" % totalpoints)

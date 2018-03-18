d27183929ba58518f751eff6c1a728f0881a5786
jackd <jacko8638@gmail.com>
2018-03-18 18:24:25 +0000

    exercise 1

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,11 +1,11 @@
 #function to take duration and process it
 def plumbMoney(duration):
-    hours = duration / 100 # takes the hours from the duration
-    mins = (duration % 100) / 10 # takes the mins from the duration and divides by ten so they can be multiplied
-    if mins >= 3:
-        charged = (hours * 40) + ((mins + 1) * 20)
+    hours = duration / 100  # takes the hours from the duration
+    mins = (duration % 100) / 10  # takes the mins from the duration and divides by ten so they can be multiplied
+    if mins >= 3:  # checks to see if an extra half an hour is needed
+        charged = (hours * 40) + ((mins + 1) * 20)  # adds extra half hour and totals charged money
     else:
-        charged = (hours * 40) + (mins * 20)
+        charged = (hours * 40) + (mins * 20)  # totals charged
     return charged
 
 
